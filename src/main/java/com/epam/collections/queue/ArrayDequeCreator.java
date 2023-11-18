3ff7697fffd7150c38865ff3d140be082c03fb47
antonio481 <adanilovx@gmail.com>
2023-11-18 15:01:21 +0400

    FinalDecisionCommit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -8,18 +8,19 @@ import java.util.Queue;
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
         ArrayDeque<Integer>resultDequeue=new ArrayDeque<>();
-        resultDequeue.add(Objects.requireNonNull(firstQueue.poll()));
-        resultDequeue.add(Objects.requireNonNull(firstQueue.poll()));
-        resultDequeue.add(Objects.requireNonNull(secondQueue.poll()));
-        resultDequeue.add(Objects.requireNonNull(secondQueue.poll()));
+        resultDequeue.add(Objects.requireNonNull(firstQueue.remove()));
+        resultDequeue.add(Objects.requireNonNull(firstQueue.remove()));
+        resultDequeue.add(Objects.requireNonNull(secondQueue.remove()));
+        resultDequeue.add(Objects.requireNonNull(secondQueue.remove()));
         while(firstQueue.size()>0&&secondQueue.size()>0){
             firstQueue.add(resultDequeue.poll());
-            resultDequeue.add(Objects.requireNonNull(firstQueue.poll()));
-            resultDequeue.add(Objects.requireNonNull(firstQueue.poll()));
-            secondQueue.add(firstQueue.poll());
-            resultDequeue.add(Objects.requireNonNull(secondQueue.poll()));
-            resultDequeue.add(Objects.requireNonNull(secondQueue.poll()));
+            resultDequeue.add(Objects.requireNonNull(firstQueue.remove()));
+            resultDequeue.add(Objects.requireNonNull(firstQueue.remove()));
+            secondQueue.add(resultDequeue.poll());
+            resultDequeue.add(Objects.requireNonNull(secondQueue.remove()));
+            resultDequeue.add(Objects.requireNonNull(secondQueue.remove()));
         }
+        System.out.println(resultDequeue);
         return resultDequeue;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,21 +1,24 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-        Queue<Integer> queue=new PriorityQueue<>();
-       if(numberOfDishes>0) {
-           for (int i = 0; i < numberOfDishes - 1; i++) {
-               if (i != everyDishNumberToEat) {
-                   queue.add(i);
-               }
-           }
-           queue.add(everyDishNumberToEat);
-       }
-        return new ArrayList<>(queue);
+        List<Integer> dishes = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+
+        int step = everyDishNumberToEat - 1;
+        int i = 0;
+        while (!dishes.isEmpty()) {
+            i = (i + step) % dishes.size();
+            int n = dishes.remove(i);
+            result.add(n);
+        }
+
+        return result;
     }
 }
